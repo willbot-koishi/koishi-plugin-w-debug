8ce106879ab2206ce5d26c77124beb32e2ce5c76
ForkΨKILLET <fork_killet@outlook.com>
2024-08-30 13:30:06 +0800

    feat: basic debug functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, SessionError } from 'koishi'
+import format from 'pretty-format'
 
 export const name = 'w-debug'
 
@@ -6,6 +7,35 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+const select = <T, const Ks extends Array<keyof T>>(value: T, keys: Ks): {
+    [K in Ks[number]]: T[K]
+} => Object.fromEntries(keys.map(key => [ key, value[key] ])) as any
+
 export function apply(ctx: Context) {
-  // write your plugin here
+    ctx.command('debug.session')
+        .action(({ session }) => JSON.stringify(session, null, 2))
+
+    ctx.command('debug.session.id')
+        .action(({ session }) => JSON.stringify(select(session, [
+            'id', 'uid', 'gid', 'cid', 'fid', 'sid',
+            'userId', 'selfId', 'guildId', 'channelId',
+            'messageId', 'roleId', 'operatorId'
+        ])))
+
+    ctx.command('debug.prefix')
+        .action(() => JSON.stringify(ctx.root.config.prefix))
+
+    ctx.command('debug.arg0 <arg0>')
+        .action((_, arg0) => arg0)
+
+    ctx.command('debug.eval <code:text>', { authority: 4 })
+        .action(async (_, code) => {
+            try {
+                return format(await eval(code))
+            }
+            catch (error) {
+                return format(error)
+            }
+        })
 }
+
